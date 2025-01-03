69b9822f3521674b955eb9e2292a4612884f026a
Agemiro Alves de Sousa Neto <agemiroalves11@gmail.com>
2025-01-03 03:20:40 +0100

    build: add body-parser dependency

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //I'm telling express to use EJS as a View engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
 app.get("/ask", (req, res) => {
   res.render("ask");
+});
+
+app.post("/savequestion", (req, res) => {
+  var title = req.body.title;
+  var description = req.body.description;
+
+  res.send(
+    "form received! Title: " + title + " - " + " Description: " + description
+  );
 });
 
 app.listen(8080, () => {
